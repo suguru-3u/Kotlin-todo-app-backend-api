209ad1b106018d2f475f9c83d9c8e2dbe0e0c552
suguru-3u <suguru.13579515@gmail.com>
2024-12-02 23:29:03 +0900

    TodoServiceクラスの作成

--- src/main/kotlin/todo/app/backend/api/presentation/controller/TodoController.kt
+++ src/main/kotlin/todo/app/backend/api/presentation/controller/TodoController.kt
@@ -2,14 +2,17 @@ package todo.app.backend.api.presentation.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import todo.app.backend.api.service.TodoApplicationService
 
 @RestController
-class TodoController {
-
+class TodoController(
+    val todoApplicationService : TodoApplicationService
+) {
     // Todo: TodoをCRUDするAPIを作成する
 
+    // 一覧取得API
     @GetMapping("/index")
     fun index(){
-        println("Hello World")
+        todoApplicationService.index()
     }
 }
---
+++ src/main/kotlin/todo/app/backend/api/service/TodoAplicationService.kt
@@ -0,0 +1,11 @@
+package todo.app.backend.api.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class TodoApplicationService {
+
+    fun index(){
+        println("Hello Service Class")
+    }
+}
